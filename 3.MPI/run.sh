b82f78f111b148e1274ee286159d13e6a8bb6c81
Gaurav Deshmukh <67900669+Cipher-007@users.noreply.github.com>
2023-05-21 22:51:47 +0530

    Update run.sh

--- 3.MPI/run.sh
+++ 3.MPI/run.sh
@@ -4,13 +4,13 @@ export MPJ_HOME=mpj/
 export PATH="$MPJ_HOME/bin:$PATH"
 
 # Compile ParallelSum.java
-javac -cp .:$MPJ_HOME/lib/mpj.jar ParallelSum.java
+javac -cp .:$MPJ_HOME/lib/mpj.jar DistributedSum.java
 
 # Check if javac compilation was successful
 
 if [ $? -eq 0 ]; then
     # Run ParallelSum using mpjrun.sh with 4 processes
-    mpjrun.sh -np 4 ParallelSum
+    mpjrun.sh -np 4 DistributedSum
 else
     echo "Error: Compilation failed."
 fi
