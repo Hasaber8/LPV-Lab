f879c005874b05702f1f0594bd142b5d8216ea42
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-05-21 16:38:39 +0530

    MPI

--- 3.MPI/run.sh
+++ 3.MPI/run.sh
@@ -1,38 +1,16 @@
-# #!/bin/bash
-
-# # Open new terminal and run the server
-# gnome-terminal --tab --title="Server" --command="bash -c 'git clone https://github.com/kevinmilner/mpj-express.git'"
-
-# sleep 10
-
-# export MPJ_HOME=mpj-express/
-# export PATH=$MPJ_HOME/bin:$PATH
-
-# javac -cp .:$MPJ_HOME/lib/mpj.jar ParallelSum.java
-
-# mpjrun.sh -np 4 ParallelSum
-
-
 #!/bin/bash
 
-# Clone the repository
-git clone https://github.com/kevinmilner/mpj-express.git
+export MPJ_HOME=mpj/
+export PATH="$MPJ_HOME/bin:$PATH"
 
-# Check if git clone was successful
+# Compile ParallelSum.java
+javac -cp .:$MPJ_HOME/lib/mpj.jar ParallelSum.java
+
+# Check if javac compilation was successful
+
 if [ $? -eq 0 ]; then
-    export MPJ_HOME=mpj-express/
-    export PATH="$MPJ_HOME/bin:$PATH"
-
-    # Compile ParallelSum.java
-    javac -cp .:$MPJ_HOME/lib/mpj.jar ParallelSum.java
-
-    # Check if javac compilation was successful
-    if [ $? -eq 0 ]; then
-        # Run ParallelSum using mpjrun.sh with 4 processes
-        mpjrun.sh -np 4 ParallelSum
-    else
-        echo "Error: Compilation failed."
-    fi
+    # Run ParallelSum using mpjrun.sh with 4 processes
+    mpjrun.sh -np 4 ParallelSum
 else
-    echo "Error: Git clone failed."
+    echo "Error: Compilation failed."
 fi
