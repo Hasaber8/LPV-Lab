3dd8f1c60cc1780ed9b0ebffc931ae313bc65204
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-05-21 23:26:01 +0530

    Refactored

--- 5.Token-ring/TokenClient1.java
+++ 5.Token-ring/TokenClient1.java
@@ -6,22 +6,20 @@ public class TokenClient1 {
         InetAddress lclhost;
         BufferedReader br;
         String str = "";
-        TokenClient12 tkcl, tkser;
-        boolean hasToken;
-        boolean setSendData;
-
+        TokenClientHelper tkcl, tkser;
+		
         while (true) {
             lclhost = InetAddress.getLocalHost();
-            tkcl = new TokenClient12(lclhost);
-            tkser = new TokenClient12(lclhost);
+            tkcl = new TokenClientHelper(lclhost);
+            tkser = new TokenClientHelper(lclhost);
+			tkcl.hasToken = true;
             // tkcl.setSendPort(9001);
             tkcl.setSendPort(9004);
             tkcl.setRecPort(8002);
             lclhost = InetAddress.getLocalHost();
             tkser.setSendPort(9000);
             if (tkcl.hasToken == true) {
-
-                System.out.println("Do you want to enter the Data –> YES/NO");
+                System.out.println("Do you want to enter the Data -> YES/NO");
                 br = new BufferedReader(new InputStreamReader(System.in));
                 str = br.readLine();
                 if (str.equalsIgnoreCase("yes")) {
@@ -30,11 +28,9 @@ public class TokenClient1 {
                     tkser.sendData();
                     tkser.setSendData = false;
                 } else if (str.equalsIgnoreCase("no")) {
-                    System.out.println("i m in else");
                     // tkcl.hasToken=false;
                     tkcl.sendData();
                     tkcl.recData();
-                    System.out.println("i m leaving else");
                 }
             } else {
                 System.out.println("ENTERING RECEIVING MODE…");
@@ -42,65 +38,4 @@ public class TokenClient1 {
             }
         }
     }
-}
-
-class TokenClient12 {
-    InetAddress lclhost;
-    int sendport, recport;
-    boolean hasToken = true;
-    boolean setSendData = false;
-    TokenClient12 tkcl, tkser;
-
-    TokenClient12(InetAddress lclhost) {
-
-        this.lclhost = lclhost;
-    }
-
-    void setSendPort(int sendport) {
-        this.sendport = sendport;
-    }
-
-    void setRecPort(int recport) {
-        this.recport = recport;
-    }
-
-    void sendData() throws Exception {
-        BufferedReader br;
-        String str = "Token";
-        DatagramSocket ds;
-        DatagramPacket dp;
-
-        if (setSendData == true) {
-            System.out.println("sending ");
-            System.out.println("Enter the Data");
-            br = new BufferedReader(new InputStreamReader(System.in));
-            str = "ClientOne….." + br.readLine();
-            System.out.println("now sending");
-
-        }
-        ds = new DatagramSocket(sendport);
-        dp = new DatagramPacket(str.getBytes(), str.length(), lclhost, sendport - 1000);
-        ds.send(dp);
-        ds.close();
-        setSendData = false;
-        hasToken = false;
-    }
-
-    void recData() throws Exception {
-        String msgstr;
-        byte buffer[] = new byte[256];
-        DatagramSocket ds;
-        DatagramPacket dp;
-        ds = new DatagramSocket(recport);
-        dp = new DatagramPacket(buffer, buffer.length);
-        ds.receive(dp);
-        ds.close();
-        msgstr = new String(dp.getData(), 0, dp.getLength());
-        System.out.println("The data is " + msgstr);
-
-        if (msgstr.equals("Token")) {
-            hasToken = true;
-        }
-    }
-
 }
--- 5.Token-ring/run.sh
+++ 5.Token-ring/run.sh
@@ -5,6 +5,6 @@ javac *.java
 
 gnome-terminal --tab --title="Server" --command="bash -c 'java TokenServer; exec bash'"
 
-gnome-terminal --tab --title="Server" --command="bash -c 'java TokenClient1; exec bash'"
+gnome-terminal --tab --title="Client1" --command="bash -c 'java TokenClient1; exec bash'"
 
-gnome-terminal --tab --title="Server" --command="bash -c 'java TokenClient2; exec bash'"
+gnome-terminal --tab --title="Client2" --command="bash -c 'java TokenClient2; exec bash'"
