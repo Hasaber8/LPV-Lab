50947dc69d47bf02d89e5c8c836a794cded29a84
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-05-21 13:21:47 +0530

    CORBA

---
+++ 2.CORBA/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Compile IDL file
+# idlj -fall HelloModule.idl
+
+# Compile Java files
+# javac *.java HelloModule/*.java
+
+# Check if orbd is running and kill if necessary
+pkill orbd
+
+sleep 1
+
+# Start orbd
+orbd -ORBInitialPort 1050&
+
+sleep 2
+
+# Start the server
+gnome-terminal --tab --title="Server" --command="bash -c 'java Server -ORBInitialPort 1050 -ORBInitialHost localhost& exec bash'"
+
+sleep 2
+
+# Open new terminal and run the client
+gnome-terminal --tab --title="Client" --command="bash -c 'java Client -ORBInitialPort 1050 -ORBInitialHost localhost; exec bash'"
